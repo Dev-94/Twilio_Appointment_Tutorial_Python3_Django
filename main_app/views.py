330b173c81123d70de71b733a074fa4d870f7b7d
Dev Govindji <friendsofdev94@gmail.com>
2019-11-14 16:43:57 -0600

    adds in imports in to views file

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
 
-def home(request):
-    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+from .models import Appointment
 
-# Create your views here.
+
+class AppointmentCreateView(SuccessMessageMixin, CreateView):
+    model = Model
+    template_name = ".html"
+
+
+# from django.shortcuts import render
+# from django.http import HttpResponse
+
+# def home(request):
+#     return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+# # Create your views here.
