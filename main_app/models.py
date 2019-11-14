cce805e14dde78589876091acf308e3e038efafc
Dev Govindji <friendsofdev94@gmail.com>
2019-11-14 16:56:10 -0600

    adds html files for appointment form and base

--- main_app/models.py
+++ main_app/models.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 import redis
 
-from django.core.exceptions import validationError
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.urls import reverse
 from django.utils.encoding import python_2_unicode_compatible
