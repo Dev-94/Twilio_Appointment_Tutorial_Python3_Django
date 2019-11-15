f76609d777601dd6b0a4cf60d599102384aeb959
Dev Govindji <friendsofdev94@gmail.com>
2019-11-15 17:36:47 -0600

    adds sms reminder function

--- main_app/tasks.py
+++ main_app/tasks.py
@@ -8,3 +8,11 @@ from twilio.rest import Client
 
 from .models import Appointment
 clinet = Client(settings.ACda05149c0b4d416716ba2ba9e217ae3b, settings.3d46e40bd95ee85114d46c387c78bc3c)
+
+
+@dramatiq.actor
+def send_sms_reminder(appointment_id):
+    try:
+        appointment = Appointment.objects.get(pk=appointment_id)
+    except Appointment.DoesNotExist:
+        return
